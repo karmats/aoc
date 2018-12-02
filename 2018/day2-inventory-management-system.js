b2222325ac1319803691d4052c66423f03145b7c
Mats Roshauw <karmats@gmail.com>
2018-12-02 09:41:30 +0100

    Day 2 - part 2

--- 2018/day2-inventory-management-system.js
+++ 2018/day2-inventory-management-system.js
@@ -21,7 +21,18 @@ const boxTwoAndThreeCount = chars => {
   );
 };
 
+const diffingChars = (a, b) => {
+    const result = [];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            result.push(a[i]);
+        }
+    }
+    return result;
+}
+
 fileToPuzzle("day2-puzzle.txt", puzzle => {
+  // Part 1
   const boxCount = puzzle.map(boxTwoAndThreeCount).reduce(
     (acc, c) => {
       if (c.two) {
@@ -34,6 +45,18 @@ fileToPuzzle("day2-puzzle.txt", puzzle => {
     },
     { twos: 0, threes: 0 }
   );
-  // Part 1
+
   console.log(boxCount.twos * boxCount.threes);
+
+  // Part 2
+  loop:
+  for (let i = 0; i < puzzle.length; i++) {
+      for (let j = i + 1; j < puzzle.length; j++) {
+          const diff = diffingChars(puzzle[i], puzzle[j]);
+          if (diff.length === 1) {
+              console.log(puzzle[i].replace(diff[0], ""));
+              break loop;
+          }
+      }
+  }
 });
