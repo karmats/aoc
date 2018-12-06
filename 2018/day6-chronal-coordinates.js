d9ca951508931f4a00b37f8c919bd3710a9d7704
Mats Roshauw <karmats@gmail.com>
2018-12-06 23:09:20 +0100

    Day 6 - part 2

--- 2018/day6-chronal-coordinates.js
+++ 2018/day6-chronal-coordinates.js
@@ -1,6 +1,6 @@
 const { fileToPuzzle } = require("./util");
 
-const HIGH_NUMBER = 99999999;
+const MAX_INT = Number.MAX_SAFE_INTEGER;
 fileToPuzzle("./day6-puzzle.txt", puzzle => {
   const coordinates = puzzle.map(p =>
     p.split(",").map(s => parseInt(s.trim()))
@@ -9,17 +9,19 @@ fileToPuzzle("./day6-puzzle.txt", puzzle => {
   const maxY = coordinates.reduce((acc, c) => (c[1] > acc ? c[1] : acc), 0);
   const minX = coordinates.reduce(
     (acc, c) => (c[0] < acc ? c[0] : acc),
-    HIGH_NUMBER
+    MAX_INT
   );
   const minY = coordinates.reduce(
     (acc, c) => (c[1] < acc ? c[1] : acc),
-    HIGH_NUMBER
+    MAX_INT
   );
 
+  const manhattanDistance = (x, y, c) =>
+    Math.abs(x - c[0]) + Math.abs(y - c[1]);
   const closest = (x, y) =>
     coordinates.reduce(
       (acc, c) => {
-        const distance = Math.abs(x - c[0]) + Math.abs(y - c[1]);
+        const distance = manhattanDistance(x, y, c);
         if (acc.distance > distance) {
           acc.coordinates = [c];
           acc.distance = distance;
@@ -28,7 +30,7 @@ fileToPuzzle("./day6-puzzle.txt", puzzle => {
         }
         return acc;
       },
-      { coordinates: [], distance: HIGH_NUMBER }
+      { coordinates: [], distance: MAX_INT }
     );
   const withinMaxMin = coordinate => {
     const x = coordinate[0];
@@ -37,8 +39,8 @@ fileToPuzzle("./day6-puzzle.txt", puzzle => {
   };
 
   let result = {};
-  for (let x = 0; x <= maxX + 200; x++) {
-    for (let y = 0; y <= maxY + 200; y++) {
+  for (let x = 0; x <= maxX; x++) {
+    for (let y = 0; y <= maxY; y++) {
       const c = closest(x, y);
       if (c.coordinates.length === 1) {
         const coords = c.coordinates[0];
@@ -64,15 +66,35 @@ fileToPuzzle("./day6-puzzle.txt", puzzle => {
     const c = closest(maxX + 1, y);
     const c2 = closest(minX - 1, y);
     if (c.coordinates.length === 1) {
-      outside.add(c.coordinates[0].join('_'));
+      outside.add(c.coordinates[0].join("_"));
     }
     if (c2.coordinates.length === 1) {
-      outside.add(c2.coordinates[0].join('_'));
+      outside.add(c2.coordinates[0].join("_"));
     }
   }
+  // Part 1
   console.log(
     Object.keys(result)
       .filter(r => !outside.has(r))
       .reduce((acc, c) => (result[c] > acc ? result[c] : acc), 0)
   );
+
+  // Part 2
+  const MAX_DISTANCE = 10000;
+  const withinAllDistances = (x, y) => {
+    let totalDistance = coordinates.reduce(
+      (acc, c) => manhattanDistance(x, y, c) + acc,
+      0
+    );
+    return totalDistance < MAX_DISTANCE ? 0 : totalDistance;
+  };
+  let count = 0;
+  for (let x = 0; x <= maxX; x++) {
+    for (let y = 0; y <= maxY; y++) {
+      if (withinAllDistances(x, y) === 0) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 });
