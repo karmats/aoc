cc77dca2a4d1be8dbc69257c0d985b5d347639e9
Mats Roshauw <karmats@gmail.com>
2018-12-12 13:42:47 +0100

    Day 11 - part 2

--- 2018/day11-chronal-charge.js
+++ 2018/day11-chronal-charge.js
@@ -1,5 +1,4 @@
-const FUEL_CELLS_X = 300;
-const FUEL_CELLS_Y = 300;
+const SIZE = 300;
 const SERIAL_NO = 7403;
 
 const power = (x, y) => {
@@ -8,31 +7,61 @@ const power = (x, y) => {
   const hundredth = parseInt(part.substring(part.length - 3, part.length - 2));
   return hundredth - 5;
 };
-const totalPower = (x, y, cells) => {
+const areaPower = (x, y, cells, size) => {
   let total = 0;
-  for (let i = y; i < y + 3; i++) {
-    for (let j = x; j < x + 3; j++) {
+  for (let i = y; i < y + size; i++) {
+    for (let j = x; j < x + size; j++) {
       total += cells[j + "_" + i];
     }
   }
   return total;
 };
+const addedPower = (x, y, cells, size) => {
+  let tot = 0;
+  for (let j = x; j <= x + size; j++) {
+    tot += cells[j + "_" + (size + y)];
+  }
+  for (let i = y; i <= y + size - 1; i++) {
+    tot += cells[(size + x) + "_" + i];
+  }
+  return tot;
+};
 let grid = {};
-for (let y = 1; y <= FUEL_CELLS_Y; y++) {
-  for (let x = 1; x <= FUEL_CELLS_X; x++) {
+for (let y = 1; y <= SIZE; y++) {
+  for (let x = 1; x <= SIZE; x++) {
     grid[x + "_" + y] = power(x, y);
   }
 }
 let highest = 0;
-let highestCoord = [1, 1];
-for (let y = 1; y < FUEL_CELLS_Y - 3; y++) {
-  for (let x = 1; x <= FUEL_CELLS_X - 3; x++) {
-      const total = totalPower(x, y, grid);
-      if (total > highest) {
-          highest = total;
-          highestCoord = [x, y];
+let highestCoord = [0, 0];
+let highestSize = 0;
+let highestPart1 = 0;
+let highestPart1Coord = [0, 0];
+const prev = {};
+for (let size = 1; size <= SIZE; size++) {
+  for (let y = 1; y <= SIZE - size; y++) {
+    for (let x = 1; x <= SIZE - size; x++) {
+      let total = prev[x + "_" + y];
+      if (total === undefined) {
+        total = areaPower(x, y, grid, size);
+      } else {
+        total += addedPower(x, y, grid, size - 1);
       }
+      prev[x + "_" + y] = total;
+      if (size === 3 && total > highestPart1) {
+        highestPart1 = total;
+        highestPart1Coord = [x, y];
+      }
+      if (total > highest) {
+        highest = total;
+        highestCoord = [x, y];
+        highestSize = size;
+      }
+    }
   }
+  //if (size === 4) break;
 }
-
-console.log(highestCoord.join(','));
+// Part 1
+console.log(highestPart1Coord.join(","));
+// Part 2
+console.log(`${highestCoord.join(",")},${highestSize}`);
