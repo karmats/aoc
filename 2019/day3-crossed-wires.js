995ad43a0bb0b6882198de1ab64874801cc3dc5f
Mats Roshauw <karmats@gmail.com>
2019-12-03 16:43:51 +0100

    2019 Day 3 - part 2

--- 2019/day3-crossed-wires.js
+++ 2019/day3-crossed-wires.js
@@ -2,34 +2,32 @@ const { fileToPuzzle } = require("./util");
 
 const regex = /(L|R|D|U)(\d*)/;
 
-const createEmptyMap = size => {
-  let res = [];
-  for (let i = 0; i < size; i++) {
-    res[i] = null;
-  }
-  return res;
-};
-const getCoords = (currentCoord, steps, direction) => {
+const createEmptyMap = size => new Array(size).fill(null);
+const getCoords = (currentCoord, steps, direction, totalSteps) => {
   switch (direction) {
     case "L":
-      return createEmptyMap(steps).map((_, idx) => ({
+      return createEmptyMap(+steps).map((_, idx) => ({
         x: currentCoord.x - (idx + 1),
-        y: currentCoord.y
+        y: currentCoord.y,
+        totalSteps: totalSteps + idx + 1
       }));
     case "R":
-      return createEmptyMap(steps).map((_, idx) => ({
+      return createEmptyMap(+steps).map((_, idx) => ({
         x: currentCoord.x + (idx + 1),
-        y: currentCoord.y
+        y: currentCoord.y,
+        totalSteps: totalSteps + idx + 1
       }));
     case "U":
-      return createEmptyMap(steps).map((_, idx) => ({
+      return createEmptyMap(+steps).map((_, idx) => ({
         x: currentCoord.x,
-        y: currentCoord.y + (idx + 1)
+        y: currentCoord.y + (idx + 1),
+        totalSteps: totalSteps + idx + 1
       }));
     case "D":
-      return createEmptyMap(steps).map((_, idx) => ({
+      return createEmptyMap(+steps).map((_, idx) => ({
         x: currentCoord.x,
-        y: currentCoord.y - (idx + 1)
+        y: currentCoord.y - (idx + 1),
+        totalSteps: totalSteps + idx + 1
       }));
     default:
       throw new Error(`Unknown direction '${direction}'`);
@@ -41,19 +39,36 @@ const spannedCoordinates = wire =>
       const regexResult = regex.exec(c);
       const direction = regexResult[1];
       const steps = regexResult[2];
-      const newCoords = getCoords(acc.currentCoord, steps, direction);
+      const newCoords = getCoords(
+        acc.currentCoord,
+        steps,
+        direction,
+        acc.totalSteps
+      );
       return {
         currentCoord: newCoords[newCoords.length - 1],
-        coords: acc.coords.concat(newCoords)
+        coords: acc.coords.concat(newCoords),
+        totalSteps: acc.totalSteps + +steps
       };
     },
     {
-      currentCoord: { x: 0, y: 0 },
-      coords: []
+      currentCoord: { x: 0, y: 0, steps: 0 },
+      coords: [],
+      totalSteps: 0
     }
   );
 const findIntersectionPoints = (arr1, arr2) =>
-  arr1.filter(a1 => arr2.find(a2 => a2.x === a1.x && a1.y === a2.y));
+  arr1.reduce((acc, c) => {
+    const match = arr2.find(a => a.x === c.x && a.y === c.y);
+    if (match) {
+      return acc.concat({
+        x: match.x,
+        y: match.y,
+        steps: c.totalSteps + match.totalSteps
+      });
+    }
+    return acc;
+  }, []);
 const manhattanDistance = coord => Math.abs(coord.x) + Math.abs(coord.y);
 fileToPuzzle("day3-puzzle.txt", puzzle => {
   const wire1 = puzzle[0].split(",");
@@ -63,18 +78,26 @@ fileToPuzzle("day3-puzzle.txt", puzzle => {
   const result = findIntersectionPoints(wire1Coords, wire2Coords).reduce(
     (acc, c) => {
       const distance = manhattanDistance(c);
-      if (acc.min === null || distance < acc.min) {
-        return {
-          min: distance,
-          coord: c
-        };
+      let minDistance = acc.minDistance;
+      let minSteps = acc.minSteps;
+      if (acc.minDistance === null || distance < acc.minDistance) {
+        minDistance = distance;
       }
-      return acc;
+      if (acc.minSteps === null || c.steps < acc.minSteps) {
+        minSteps = c.steps;
+      }
+      return {
+        minDistance: minDistance,
+        coord: c,
+        minSteps: minSteps
+      };
     },
     {
-      min: null,
+      minDistance: null,
+      minSteps: null,
       coord: null
     }
   );
-  console.log(result.min);
+  console.log(result.minDistance);
+  console.log(result.minSteps);
 });
