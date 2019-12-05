13af47f3f5391ee359e372812113edd59f141181
Mats Roshauw <karmats@gmail.com>
2019-12-05 21:29:16 +0100

    2019 Day 5 - part 2

--- 2019/day5-sunny-chance-asteroids.js
+++ 2019/day5-sunny-chance-asteroids.js
@@ -5,25 +5,32 @@ const runIntCode = (list, pos, input, output) => {
   const op = +opCode.slice(2);
   const modeParamA = +opCode[1];
   const modeParamB = +opCode[0];
-  const paramA = list[pos + 1];
-  const paramB = list[pos + 2];
+  const posA = list[pos + 1];
+  const posB = list[pos + 2];
+  const paramA = modeParamA === 1 ? posA : list[posA];
+  const paramB = modeParamB === 1 ? posB : list[posB];
   const resPos = list[pos + 3];
   if (op === 1) {
-    list[resPos] =
-      (modeParamA === 1 ? paramA : list[paramA]) +
-      (modeParamB === 1 ? paramB : list[paramB]);
+    list[resPos] = paramA + paramB;
     return runIntCode(list, pos + 4, input, output);
   } else if (op === 2) {
-    list[resPos] =
-      (modeParamA === 1 ? paramA : list[paramA]) *
-      (modeParamB === 1 ? paramB : list[paramB]);
+    list[resPos] = paramA * paramB;
     return runIntCode(list, pos + 4, input, output);
   } else if (op === 3) {
-    list[paramA] = input;
+    list[posA] = input;
     return runIntCode(list, pos + 2, input, output);
   } else if (op === 4) {
-    const newOutput = modeParamA === 1 ? paramA : list[paramA];
-    return runIntCode(list, pos + 2, input, newOutput);
+    return runIntCode(list, pos + 2, input, paramA);
+  } else if (op === 5) {
+    return runIntCode(list, paramA !== 0 ? paramB : pos + 3, input, output);
+  } else if (op === 6) {
+    return runIntCode(list, paramA === 0 ? paramB : pos + 3, input, output);
+  } else if (op === 7) {
+    list[resPos] = paramA < paramB ? 1 : 0;
+    return runIntCode(list, pos + 4, input, output);
+  } else if (op === 8) {
+    list[resPos] = paramA === paramB ? 1 : 0;
+    return runIntCode(list, pos + 4, input, output);
   } else if (op === 99) {
     return output;
   }
@@ -33,7 +40,8 @@ const runIntCode = (list, pos, input, output) => {
 fileToPuzzle(
   "day5-puzzle.txt",
   puzzle => {
-    console.log(runIntCode(puzzle, 0, 1));
+    console.log(runIntCode(puzzle.slice(), 0, 1));
+    console.log(runIntCode(puzzle.slice(), 0, 5));
   },
   { separator: ",", isNumber: true }
 );
