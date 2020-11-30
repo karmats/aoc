657833ab532d3fdbc02eecbb66d60eeea8c47ccf
Mats Roshauw <karmats@gmail.com>
2020-11-30 10:23:19 +0100

    2020 prep

---
+++ 2020/util.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+const fileToPuzzle = (
+  filename,
+  callback,
+  options = { separator: "\n", isNumber: false }
+) => {
+  fs.readFile(`${__dirname}/${filename}`, "utf-8", (_, data) => {
+    const result = data
+      .split(options.separator || "\n")
+      .map(d => (options.isNumber ? parseInt(d) : d));
+    callback(result);
+  });
+};
+
+const leftPad = (num, padding) => {
+  while(num.length < padding) {
+    num = '0' + num;
+  }
+  return num;
+}
+
+module.exports = { fileToPuzzle, leftPad };
