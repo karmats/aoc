0edfb1d876cb54ed4b133dda3850f6d41e1f3fb6
Mats Roshauw <karmats@gmail.com>
2020-12-05 13:10:36 +0100

    2020 Day 5 - part 2

--- 2020/day5-binary-boarding.js
+++ 2020/day5-binary-boarding.js
@@ -1,5 +1,8 @@
 const { fileToPuzzle } = require("./util");
 
+const MAX_ROW = 127;
+const MAX_COL = 7;
+
 const toSeat = (bsp) =>
   bsp.split("").reduce(
     (position, move) => {
@@ -26,14 +29,27 @@ const toSeat = (bsp) =>
       }
     },
     [
-      [0, 127],
-      [0, 7],
+      [0, MAX_ROW],
+      [0, MAX_COL],
     ]
   );
 
 const getSeatId = (seat) => seat[0] * 8 + seat[1];
+
+const getAllSeatIds = () => {
+  let allSeats = [];
+  for (let row = 0; row < MAX_ROW; row++) {
+    for (let col = 0; col < MAX_COL; col++) {
+      allSeats.push(getSeatId([row, col]));
+    }
+  }
+  return allSeats;
+};
+
 fileToPuzzle("day5-puzzle.txt", (puzzle) => {
   const seats = puzzle.map(toSeat);
+
+  // Part 1
   const highest = seats.reduce((max, seat) => {
     const seatId = getSeatId(seat);
     if (seatId > max) {
@@ -42,4 +58,10 @@ fileToPuzzle("day5-puzzle.txt", (puzzle) => {
     return max;
   }, 0);
   console.log(highest);
+
+  // Part 2
+  const allSeatIds = getAllSeatIds().sort((a, b) => a - b);
+  const seatIds = seats.map(getSeatId).sort((a, b) => a - b);
+  const lowest = seatIds.reduce((min, seatId) => (seatId < min ? seatId : min), highest);
+  console.log(allSeatIds.filter((id) => id < highest && id > lowest && !seatIds.includes(id)).pop());
 });
