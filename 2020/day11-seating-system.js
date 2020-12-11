b6b696fb766aece8a0081ab86a441c10a8b6b1af
Mats Roshauw <karmats@gmail.com>
2020-12-11 14:10:14 +0100

    2020 Day 11 - Part 2

--- 2020/day11-seating-system.js
+++ 2020/day11-seating-system.js
@@ -1,35 +1,159 @@
 const { fileToPuzzle } = require("./util");
 
-const getNewSeating = (rowIdx, colIdx, seates) => {
-  const currentSeating = seates[rowIdx][colIdx];
-  let occupies = 0;
-  for (let i = rowIdx - 1; i <= rowIdx + 1; i++) {
-    const row = seates[i];
-    if (row) {
-      for (let j = colIdx - 1; j <= colIdx + 1; j++) {
-        if (j === colIdx && i === rowIdx) {
-          continue;
+const MODE = {
+  PART_1: 1,
+  PART_2: 2,
+};
+
+const DIRECTIONS = {
+  left: "left",
+  topLeft: "top-left",
+  top: "top",
+  topRight: "top-right",
+  right: "right",
+  bottomRight: "bottom-right",
+  bottom: "bottom",
+  bottomLeft: "bottom-left",
+};
+
+const firstSeenStatus = (rowIdx, colIdx, seates, direction) => {
+  let ci = direction.includes("left") ? colIdx - 1 : colIdx + 1;
+  let ri = direction.includes("top") ? rowIdx - 1 : rowIdx + 1;
+  const maxRow = seates.length;
+  const maxCol = seates[0].length;
+  switch (direction) {
+    case DIRECTIONS.left:
+      for (; ci >= 0; ci--) {
+        let status = seates[rowIdx][ci] || ".";
+        if (status !== ".") {
+          return status;
         }
-        if (row[j] === "#") {
-          occupies++;
+      }
+      return ".";
+    case DIRECTIONS.topLeft:
+      for (; ri >= 0, ci >= 0; ri--, ci--) {
+        const row = seates[ri];
+        if (row) {
+          let status = row[ci] || ".";
+          if (status !== ".") {
+            return status;
+          }
+        }
+      }
+      return ".";
+    case DIRECTIONS.top:
+      for (; ri >= 0; ri--) {
+        const row = seates[ri];
+        let status = row[colIdx] || ".";
+        if (status !== ".") {
+          return status;
+        }
+      }
+      return ".";
+    case DIRECTIONS.topRight:
+      for (; ri >= 0, ci < maxCol; ri--, ci++) {
+        const row = seates[ri];
+        if (row) {
+          let status = row[ci] || ".";
+          if (status !== ".") {
+            return status;
+          }
+        }
+      }
+      return ".";
+    case DIRECTIONS.right:
+      for (; ci < maxCol; ci++) {
+        const status = seates[rowIdx][ci] || ".";
+        if (status !== ".") {
+          return status;
+        }
+      }
+      return ".";
+    case DIRECTIONS.bottomRight:
+      for (; ri < maxRow, ci < maxCol; ri++, ci++) {
+        const row = seates[ri];
+        if (row) {
+          let status = row[ci] || ".";
+          if (status !== ".") {
+            return status;
+          }
+        }
+      }
+      return ".";
+    case DIRECTIONS.bottom:
+      for (; ri < maxRow; ri++) {
+        const row = seates[ri];
+        if (row) {
+          let status = row[colIdx] || ".";
+          if (status !== ".") {
+            return status;
+          }
+        }
+      }
+      return ".";
+    case DIRECTIONS.bottomLeft:
+      for (; ri < maxRow, ci >= 0; ri++, ci--) {
+        const row = seates[ri];
+        if (row) {
+          let status = row[ci] || ".";
+          if (status !== ".") {
+            return status;
+          }
+        }
+      }
+      return ".";
+    default:
+      throw new Error(`WTF is ${direction}`);
+  }
+};
+
+const getNewSeating = (rowIdx, colIdx, seates, mode) => {
+  const currentSeating = seates[rowIdx][colIdx];
+  if (currentSeating === ".") {
+    return currentSeating;
+  }
+  let occupies = 0;
+  if (mode === MODE.PART_1) {
+    for (let i = rowIdx - 1; i <= rowIdx + 1; i++) {
+      const row = seates[i];
+      if (row) {
+        for (let j = colIdx - 1; j <= colIdx + 1; j++) {
+          if (j === colIdx && i === rowIdx) {
+            continue;
+          }
+          if (row[j] === "#") {
+            occupies++;
+          }
         }
       }
     }
-  }
-  if (currentSeating === "L" && occupies === 0) {
-    return "#";
-  } else if (currentSeating === "#" && occupies >= 4) {
-    return "L";
+    if (currentSeating === "L" && occupies === 0) {
+      return "#";
+    } else if (currentSeating === "#" && occupies >= 4) {
+      return "L";
+    }
+  } else {
+    Object.values(DIRECTIONS).forEach((direction) => {
+      const status = firstSeenStatus(rowIdx, colIdx, seates, direction);
+      if (status === "#") {
+        occupies++;
+      }
+    });
+    if (currentSeating === "L" && occupies === 0) {
+      return "#";
+    } else if (currentSeating === "#" && occupies >= 5) {
+      return "L";
+    }
   }
   return currentSeating;
 };
-const getSeating = (seates) => {
+const getSeating = (seates, mode) => {
   const newSeates = [];
   for (let rowIdx = 0; rowIdx < seates.length; rowIdx++) {
     let newRow = "";
     const row = seates[rowIdx];
     for (let colIdx = 0; colIdx < row.length; colIdx++) {
-      newRow += getNewSeating(rowIdx, colIdx, seates);
+      newRow += getNewSeating(rowIdx, colIdx, seates, mode);
     }
     newSeates.push(newRow);
   }
@@ -44,12 +168,12 @@ const equals = (seating1, seating2) => {
   return true;
 };
 
-const getConstantSeating = (initialSeating) => {
+const getConstantSeating = (initialSeating, mode) => {
   let currentSeating = initialSeating.slice();
-  let newSeating = getSeating(currentSeating);
+  let newSeating = getSeating(currentSeating, mode);
   while (true) {
     currentSeating = newSeating;
-    newSeating = getSeating(currentSeating);
+    newSeating = getSeating(currentSeating, mode);
     if (equals(currentSeating, newSeating)) {
       break;
     }
@@ -62,6 +186,11 @@ const toString = (seates) => seates.reduce((str, c) => `${str}\n${c}`);
 const caculateOccupiedSeates = (seates) => seates.reduce((occupied, row) => occupied + (row.match(/#/g) || []).length, 0);
 
 fileToPuzzle("day11-puzzle.txt", (puzzle) => {
-  const constantSeating = getConstantSeating(puzzle);
-  console.log(caculateOccupiedSeates(constantSeating));
+  // Part 1
+  const constantSeating1 = getConstantSeating(puzzle, MODE.PART_1);
+  console.log(caculateOccupiedSeates(constantSeating1));
+
+  // Part 2
+  const constantSeating2 = getConstantSeating(puzzle, MODE.PART_2);
+  console.log(caculateOccupiedSeates(constantSeating2));
 });
