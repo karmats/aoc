c6576d426b5c58ae92ede11991458770c2d0bd89
Mats Roshauw <karmats@gmail.com>
2021-04-19 12:50:46 +0200

    2015 Day 6 - Part 2

--- 2015/day6-probably-a-fire-hazard.js
+++ 2015/day6-probably-a-fire-hazard.js
@@ -42,16 +42,56 @@ const doLights = (instructions) => {
   return lightGrid;
 };
 
+const adjustBrightness = (row, start, end, brightness) => {
+  const replacement = row.slice(start, end).reduce((acc, currBrightness) => {
+    const newBrightness = currBrightness + brightness <= 0 ? 0 : currBrightness + brightness;
+    return acc.concat(newBrightness);
+  }, []);
+  return row.slice(0, start).concat(replacement).concat(row.slice(end));
+};
+
+const doLights2 = (instructions) => {
+  let lightGrid = Array.from(new Array(1000)).map(() => Array.from(new Array(1000)).map(() => 0));
+  instructions.forEach((instruction) => {
+    let brightness = 0;
+    switch (instruction.method) {
+      case "turn on":
+        brightness = 1;
+        break;
+      case "turn off":
+        brightness = -1;
+        break;
+      case "toggle":
+        brightness = 2;
+        break;
+      default:
+        throw new Error(`No such method '${method}'`);
+    }
+    for (let i = instruction.yFrom; i <= instruction.yTo; i++) {
+      lightGrid[i] = adjustBrightness(lightGrid[i], instruction.xFrom, instruction.xTo + 1, brightness);
+      if (lightGrid[i].length !== 1000) {
+        throw new Error(`WTF!! ${lightGrid[i].length}`);
+      }
+    }
+  });
+  return lightGrid;
+};
+
 const countOnLights = (row) => (row.match(/\*/g) || []).length;
+const countBrightness = (row) => row.reduce((count, brightness) => count + +brightness, 0);
 
 fileToPuzzle("day6-puzzle.txt", (puzzle) => {
-  //const puzzle = ["turn on 0,0 through 2,2", "toggle 1,1 through 2,2"];
   const instructions = puzzle.map((p) => {
     const regexResult = INSTRUCTION_REGEX.exec(p);
     const [, method, xFrom, yFrom, xTo, yTo] = regexResult;
     return { method, xFrom: +xFrom, xTo: +xTo, yFrom: +yFrom, yTo: +yTo };
   });
-  const lightingGrid = doLights(instructions);
 
+  // Part 1
+  const lightingGrid = doLights(instructions);
   console.log(lightingGrid.reduce((onLights, row) => onLights + countOnLights(row), 0));
+
+  // Part 2
+  const lightGrid2 = doLights2(instructions);
+  console.log(lightGrid2.reduce((brightness, row) => brightness + countBrightness(row), 0));
 });
