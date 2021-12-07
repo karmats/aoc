00a62ff5af9703a50d7b2ff20f1c15aeca4522d4
Mats Roshauw <karmats@gmail.com>
2021-12-07 16:31:55 +0100

    2021 Day 6 - Part 2

--- 2021/day6-lanternfish.js
+++ 2021/day6-lanternfish.js
@@ -1,21 +1,30 @@
-const { fileToPuzzle } = require("./util");
+const { fileToPuzzle, sum } = require("./util");
 
-doCycles = (days, ages) => {
-  for (let i = 0; i < days; i++) {
-    const zeroes = ages.filter((a) => a === 0);
-    ages = ages.map((age) => (age === 0 ? 6 : age - 1));
-    zeroes.forEach(() => {
-      ages.push(8);
-    });
+const countAges = (ages, age) => ages.filter((a) => age === a).length;
+
+const doCycles = (days, ages) => {
+  let fishAges = [];
+  for (let age = 0; age <= 8; age++) {
+    fishAges.push(countAges(ages, age));
   }
-  return ages;
+  for (let i = 0; i < days; i++) {
+    const fishReady = fishAges[0];
+    for (let j = 0; j < fishAges.length - 1; j++) {
+      fishAges[j] = fishAges[j + 1];
+    }
+    fishAges[fishAges.length - 1] = fishReady;
+    fishAges[6] = fishAges[6] + fishReady;
+  }
+  return fishAges;
 };
 
 fileToPuzzle(
   "day6-puzzle.txt",
   (puzzle) => {
     // Part 1
-    console.log(doCycles(80, puzzle).length);
+    console.log(sum(doCycles(80, puzzle)));
+    // Part 2
+    console.log(sum(doCycles(256, puzzle)));
   },
   {
     separator: ",",
--- 2021/util.js
+++ 2021/util.js
@@ -1,27 +1,23 @@
 const fs = require("fs");
 
-const fileToPuzzle = (
-  filename,
-  callback,
-  options = { separator: "\n", isNumber: false }
-) => {
+const fileToPuzzle = (filename, callback, options = { separator: "\n", isNumber: false }) => {
   fs.readFile(`${__dirname}/${filename}`, "utf-8", (_, data) => {
-    const result = data
-      .split(options.separator || "\n")
-      .map(d => (options.isNumber ? +d : d));
+    const result = data.split(options.separator || "\n").map((d) => (options.isNumber ? +d : d));
     callback(result);
   });
 };
 
 const leftPad = (num, padding) => {
-  while(num.length < padding) {
-    num = '0' + num;
+  while (num.length < padding) {
+    num = "0" + num;
   }
   return num;
-}
+};
 
 const replaceAtIndex = (string, index, replacement) => {
   return string.substr(0, index) + replacement + string.substr(index + replacement.length);
-}
+};
 
-module.exports = { fileToPuzzle, leftPad, replaceAtIndex };
+const sum = (arr) => arr.reduce((acc, c) => acc + c, 0);
+
+module.exports = { fileToPuzzle, leftPad, replaceAtIndex, sum };
