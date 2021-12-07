8fd01a30a0de1367b8f1e478a6e6ac46c849f0d3
Mats Roshauw <karmats@gmail.com>
2021-12-07 12:39:17 +0100

    2021 Day 5 - Part 2

--- 2021/day5-hydrothermal-venture.js
+++ 2021/day5-hydrothermal-venture.js
@@ -35,18 +35,41 @@ const createCoords = (vents) => {
   );
 };
 
-const doVent = (vent, coords) => {
-  for (let y = Math.min(vent.y1, vent.y2); y <= Math.max(vent.y1, vent.y2); y++) {
-    for (let x = Math.min(vent.x1, vent.x2); x <= Math.max(vent.x1, vent.x2); x++) {
-      let val = coords[y][x];
-      if (val === "x") {
-        coords[y] = replaceAtIndex(coords[y], x, "1");
+const doCoordHit = (x, y, coords) => {
+  let val = coords[y][x];
+  let replace = "1";
+  if (val !== "x") {
+    val = +val;
+    replace = `${val + 1}`;
+  }
+  coords[y] = replaceAtIndex(coords[y], x, replace);
+  return coords;
+};
+const doVent = (vent, coords, diagonal = false) => {
+  const [maxX, maxY] = [Math.max(vent.x1, vent.x2), Math.max(vent.y1, vent.y2)];
+  const [minX, minY] = [Math.min(vent.x1, vent.x2), Math.min(vent.y1, vent.y2)];
+  if (diagonal && Math.abs(vent.x1 - vent.x2) === Math.abs(vent.y1 - vent.y2)) {
+    let x = vent.y1 === minY ? vent.x1 : vent.x2;
+    for (let y = minY; y <= maxY; y++) {
+      doCoordHit(x, y, coords);
+      if (vent.y1 === minY) {
+        x = vent.x1 - vent.x2 > 0 ? x - 1 : x + 1;
       } else {
-        val = +val;
-        coords[y] = replaceAtIndex(coords[y], x, `${val + 1}`);
+        x = vent.x1 - vent.x2 > 0 ? x + 1 : x - 1;
       }
     }
+  } else if (vent.x1 === vent.x2) {
+    const x = vent.x1;
+    for (let y = minY; y <= maxY; y++) {
+      doCoordHit(x, y, coords);
+    }
+  } else if (vent.y1 === vent.y2) {
+    const y = vent.y1;
+    for (let x = minX; x <= maxX; x++) {
+      doCoordHit(x, y, coords);
+    }
   }
+
   return coords;
 };
 
@@ -60,11 +83,19 @@ const countOverlaps = (coords) =>
   }, 0);
 
 fileToPuzzle("day5-puzzle.txt", (puzzle) => {
-  const vents = toVents(puzzle).filter((pos) => pos.x1 === pos.x2 || pos.y1 === pos.y2);
+  const vents = toVents(puzzle);
   let coords = createCoords(vents);
 
+  // Part 1
   vents.forEach((vent) => {
     doVent(vent, coords);
+  });
+  console.log(countOverlaps(coords));
+
+  // Part 2
+  coords = createCoords(vents);
+  vents.forEach((vent) => {
+    doVent(vent, coords, true);
   });
   console.log(countOverlaps(coords));
 });
