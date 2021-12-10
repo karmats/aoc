1bed0d93ea7a858c5e3285770ded38dcfc19adff
Mats Roshauw <karmats@gmail.com>
2021-12-10 12:07:26 +0100

    2021 Day 9 - Part 2

--- 2021/day9-smoke-basin.js
+++ 2021/day9-smoke-basin.js
@@ -13,17 +13,65 @@ const findLowest = (points) => {
         points[y + 1] ? points[y + 1][x] ?? 10 : 10,
       ].map(Number);
       if (c < left && c < top && c < right && c < bottom) {
-        lowestPoints.push(c);
+        lowestPoints.push({ value: c, point: [x, y] });
       }
     }
   }
   return lowestPoints;
 };
 
+function findNeighbors(nodes, curr) {
+  const neighbors = [];
+  const [x, y] = curr.point;
+  if (y >= 1) {
+    neighbors.push(nodes[y - 1][x]);
+  }
+  if (x >= 1) {
+    neighbors.push(nodes[y][x - 1]);
+  }
+  if (y < nodes.length - 1) {
+    neighbors.push(nodes[y + 1][x]);
+  }
+  if (x < nodes[y].length - 1) {
+    neighbors.push(nodes[y][x + 1]);
+  }
+  return neighbors;
+}
+
+function getBasinSize(nodes, current) {
+  let size = 0;
+  const queue = [current];
+  while (queue.length > 0) {
+    const curr = queue.shift();
+    if (curr.visited || curr.value === 9) {
+      continue;
+    }
+    curr.visited = true;
+    queue.push(...findNeighbors(nodes, curr));
+    size++;
+  }
+  return size;
+}
+
 fileToPuzzle("day9-puzzle.txt", (puzzle) => {
   // Part 1
-  const lowestPoints = findLowest(puzzle);
-  console.log(sum(lowestPoints.map((p) => p + 1)));
+  const lowestNodes = findLowest(puzzle);
+  console.log(sum(lowestNodes.map((p) => p.value + 1)));
 
   // Part 2
+  const nodes = puzzle.reduce((acc, c, y) => {
+    acc[y] = [];
+    for (let x = 0; x < c.length; x++) {
+      const value = +c[x];
+      acc[y].push({ point: [x, y], value, visited: false });
+    }
+    return acc;
+  }, []);
+
+  const sizes = [];
+  lowestNodes.forEach((node) => {
+    sizes.push(getBasinSize(nodes, node) - 1);
+  });
+  const [first, second, third] = sizes.sort((a, b) => b - a);
+  console.log(first * second * third);
 });
