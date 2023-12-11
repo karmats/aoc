d455e7ed9d6bd5953ff26ba760ba5dafe8a39555
Mats Roshauw <mats.roshauw@appva.com>
2023-12-11 12:13:31 +0100

    2023 Day 8 - Part 2

--- 2023/day8-haunted-wasteland.mjs
+++ 2023/day8-haunted-wasteland.mjs
@@ -1,6 +1,7 @@
-import { fileToPuzzle } from "./util.mjs";
+import { fileToPuzzle, findMin } from "./util.mjs";
 
 const MAP_REGEX = /^([A-Z]{3})\s=\s\(([A-Z]{3}),\s([A-Z]{3})\)/;
+
 fileToPuzzle("day8-puzzle.txt", (puzzle) => {
   const [instructions, ...mapArray] = puzzle;
   const map = mapArray.reduce((m, i) => {
@@ -12,13 +13,27 @@ fileToPuzzle("day8-puzzle.txt", (puzzle) => {
     return m;
   }, {});
 
-  let currentPosition = "AAA";
-  for (let steps = 0; ; steps++) {
-    const idx = steps % instructions.length;
-    const direction = instructions[idx];
-    currentPosition = direction === "L" ? map[currentPosition][0] : map[currentPosition][1];
-    if (currentPosition === "ZZZ") {
-      console.log(steps + 1);
+  const getSteps = (initialPosition = "AAA", checkOnlyLast = false) => {
+    for (let steps = 0; ; steps++) {
+      const idx = steps % instructions.length;
+      const direction = instructions[idx];
+      initialPosition = direction === "L" ? map[initialPosition][0] : map[initialPosition][1];
+      if (checkOnlyLast ? initialPosition.endsWith("Z") : initialPosition === "ZZZ") {
+        return steps + 1;
+      }
+    }
+  };
+
+  // Part 1
+  console.log(getSteps());
+
+  // Part 2
+  let currentPositions = Object.keys(map).filter((a) => a.endsWith("A"));
+  const minimumSteps = currentPositions.map((cp) => getSteps(cp, true));
+  for (let i = findMin(minimumSteps); ; i++) {
+    const nums = minimumSteps.map((s) => i % s);
+    if (nums.every((n) => n === 0)) {
+      console.log(i);
       break;
     }
   }
