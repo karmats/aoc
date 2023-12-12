33ecb5333f467f45c30cb17fd09d37001c01a6e7
Mats Roshauw <mats.roshauw@appva.com>
2023-12-12 09:57:42 +0100

    2023 Day 9 - Part 2

--- 2023/day9-mirage-maintenance.mjs
+++ 2023/day9-mirage-maintenance.mjs
@@ -1,7 +1,7 @@
 import { fileToPuzzle, sum } from "./util.mjs";
 
-const getPredictions = (arr, acc) => {
-  acc.push(arr[arr.length - 1]);
+const getPredictions = (arr, acc, previous = false) => {
+  acc.push(previous ? arr[0] : arr[arr.length - 1]);
   const first = arr[0];
   if (arr.every((a) => a === first)) {
     return acc;
@@ -10,12 +10,18 @@ const getPredictions = (arr, acc) => {
   for (let i = 0; i < arr.length - 1; i++) {
     newArr.push(arr[i + 1] - arr[i]);
   }
-  return getPredictions(newArr, acc);
+  return getPredictions(newArr, acc, previous);
 };
 
 fileToPuzzle("day9-puzzle.txt", (input) => {
   const report = input.map((i) => i.split(" ").map(Number));
 
+  // Part 1
   const nextValues = report.map((arr) => sum(getPredictions(arr, [])));
   console.log(sum(nextValues));
+
+  // Part 2
+  const previousValues = report.map((arr) => getPredictions(arr, [], true).reverse());
+  const pVal = previousValues.map((p) => p.reduce((acc, c) => c - acc, 0));
+  console.log(sum(pVal));
 });
